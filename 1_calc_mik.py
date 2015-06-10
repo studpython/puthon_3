addb63486bf5be4b4fdbb1f94ce60e3a1d10a068
studpython <student_python@ukr.net>
2015-06-10 12:49:37 +0300

    calculator lesson 1

---
+++ 1_calc_mik.py
@@ -0,0 +1,25 @@
+s = '1'
+while s == '1':
+    print ('input 1 number')
+    a = int (input())
+    print ('input +-*/**// or q for exit')
+    b = input ()
+    if b == 'q':
+        break
+    print ('input 2 number')
+    c = int (input())
+    if b == '+':
+        print (a+c)
+    elif b == '-':
+        print (a-c)
+    elif b == '*':
+        print (a*c)
+    elif b == '**':
+        print (a**c)
+    elif b == '/' or b == '//':
+        if c == int(0):
+            print ('null')
+        elif b == '/':
+            print (a/c)
+        elif b == '//':
+            print (a//c)
